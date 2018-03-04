878db245aed9a8e0ea197536bcc8711e5fb012dc
George Dobrin <georgedobrin11@gmail.com>
2018-03-04 02:03:08 +0200

    Server side done

---
+++ app.js
@@ -0,0 +1,19 @@
+var express=require('express');
+var app=express();
+var request=require('request');
+
+//routing with request npm
+
+app.get("/",function(req,res){
+    request("http://www.json-generator.com/api/json/get/bVgOzSnTci?indent=2",function(error,request,body){
+        if(error){
+			console.log('error:', error); // Print the error if one occurred
+			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+		}
+		else{
+			var results=JSON.parse(body);
+			var items=results["items"];
+			res.render("home.ejs",{items: items});
+		}
+    })
+})
